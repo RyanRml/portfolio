344fdd87a49b0e9e2f35d62d5d7d6746e9ce0a09
Ryan <ramoulryan@gmail.com>
2023-03-13 17:26:41 +0100

    form contact removed

--- components/About.js
+++ components/About.js
@@ -1,31 +1,180 @@
-import styles from "@/styles/About.module.css";
-import { useEffect, useRef, useState } from "react";
+import styles from "@/styles/About.module.css"
+import { useEffect, useRef, useState } from "react"
 
 export default function About() {
-  const [styleTest, setStyleTest] = useState({});
-  const titleRef = useRef();
+  const [titleClass, setTitleClass] = useState(`${styles.sectionTitle}`)
+  const [title2Class, setTitle2Class] = useState(`${styles.sectionTitle2}`)
+  const [aboutMeClass, setAboutMeClass] = useState(`${styles.aboutMe}`)
+
+  const [reactClass, setReactClass] = useState(
+    `${styles.skillCard} ${styles.skillCard1}`
+  )
+  const [nextClass, setNextClass] = useState(
+    `${styles.skillCard} ${styles.skillCard2}`
+  )
+  const [nodeClass, setNodeClass] = useState(
+    `${styles.skillCard} ${styles.skillCard3}`
+  )
+  const [expressClass, setExpressClass] = useState(
+    `${styles.skillCard} ${styles.skillCard4}`
+  )
+  const [htmlClass, setHtmlClass] = useState(
+    `${styles.skillCard} ${styles.skillCard5}`
+  )
+  const [cssClass, setCssClass] = useState(
+    `${styles.skillCard} ${styles.skillCard6}`
+  )
+  const [gitClass, setGitClass] = useState(
+    `${styles.skillCard} ${styles.skillCard7}`
+  )
+  const [mongoClass, setMongoClass] = useState(
+    `${styles.skillCard} ${styles.skillCard8}`
+  )
+
+  const titleRef = useRef()
+  const title2Ref = useRef()
+  const aboutMeRef = useRef()
+
+  const reactRef = useRef()
+  const nextRef = useRef()
+  const NodeRef = useRef()
+  const expressRef = useRef()
+  const htmlRef = useRef()
+  const cssRef = useRef()
+  const gitRef = useRef()
+  const mongoRef = useRef()
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        console.log(entries);
-        if (entries[0].isIntersecting) {
-          setStyleTest({ opacity: 1 });
-        } else {
-          setStyleTest({});
-        }
+        entries.forEach((entry) => {
+          if (entry.target === titleRef.current) {
+            if (entry.isIntersecting) {
+              setTitleClass(`${styles.sectionTitle} ${styles.positionBase}`)
+            } else {
+              setTitleClass(`${styles.sectionTitle}`)
+            }
+          }
+
+          if (entry.target === aboutMeRef.current) {
+            if (entry.isIntersecting) {
+              setAboutMeClass(`${styles.aboutMe} ${styles.positionBase}`)
+            } else {
+              setAboutMeClass(`${styles.aboutMe}`)
+            }
+          }
+
+          if (entry.target === title2Ref.current) {
+            if (entry.isIntersecting) {
+              setTitle2Class(`${styles.sectionTitle2} ${styles.positionBase}`)
+            } else {
+              setTitle2Class(`${styles.sectionTitle2}`)
+            }
+          }
+
+          if (entry.target === reactRef.current) {
+            if (entry.isIntersecting) {
+              setReactClass(
+                `${styles.skillCard} ${styles.skillCard1} ${styles.positionBase}`
+              )
+            } else {
+              setReactClass(`${styles.skillCard} ${styles.skillCard1}`)
+            }
+          }
+
+          if (entry.target === nextRef.current) {
+            if (entry.isIntersecting) {
+              setNextClass(
+                `${styles.skillCard} ${styles.skillCard2} ${styles.positionBase}`
+              )
+            } else {
+              setNextClass(`${styles.skillCard} ${styles.skillCard2}`)
+            }
+          }
+
+          if (entry.target === NodeRef.current) {
+            if (entry.isIntersecting) {
+              setNodeClass(
+                `${styles.skillCard} ${styles.skillCard3} ${styles.positionBase}`
+              )
+            } else {
+              setNodeClass(`${styles.skillCard} ${styles.skillCard3}`)
+            }
+          }
+
+          if (entry.target === expressRef.current) {
+            if (entry.isIntersecting) {
+              setExpressClass(
+                `${styles.skillCard} ${styles.skillCard4} ${styles.positionBase}`
+              )
+            } else {
+              setExpressClass(`${styles.skillCard} ${styles.skillCard4}`)
+            }
+          }
+
+          if (entry.target === htmlRef.current) {
+            if (entry.isIntersecting) {
+              setHtmlClass(
+                `${styles.skillCard} ${styles.skillCard5} ${styles.positionBase}`
+              )
+            } else {
+              setHtmlClass(`${styles.skillCard} ${styles.skillCard5}`)
+            }
+          }
+
+          if (entry.target === cssRef.current) {
+            if (entry.isIntersecting) {
+              setCssClass(
+                `${styles.skillCard} ${styles.skillCard6} ${styles.positionBase}`
+              )
+            } else {
+              setCssClass(`${styles.skillCard} ${styles.skillCard6}`)
+            }
+          }
+
+          if (entry.target === gitRef.current) {
+            if (entry.isIntersecting) {
+              setGitClass(
+                `${styles.skillCard} ${styles.skillCard7} ${styles.positionBase}`
+              )
+            } else {
+              setGitClass(`${styles.skillCard} ${styles.skillCard7}`)
+            }
+          }
+
+          if (entry.target === mongoRef.current) {
+            if (entry.isIntersecting) {
+              setMongoClass(
+                `${styles.skillCard} ${styles.skillCard8} ${styles.positionBase}`
+              )
+            } else {
+              setMongoClass(`${styles.skillCard} ${styles.skillCard8}`)
+            }
+          }
+        })
       },
-      { root: null, rootMargin: "0px 0px -20% 0px", threshold: 0 }
-    );
-    observer.observe(titleRef.current);
-  }, []);
+      { root: null, rootMargin: "-20% 0px -20% 0px", threshold: 0 }
+    )
+
+    observer.observe(titleRef.current)
+    observer.observe(title2Ref.current)
+    observer.observe(aboutMeRef.current)
+    observer.observe(reactRef.current)
+    observer.observe(nextRef.current)
+    observer.observe(NodeRef.current)
+    observer.observe(expressRef.current)
+    observer.observe(htmlRef.current)
+    observer.observe(cssRef.current)
+    observer.observe(gitRef.current)
+    observer.observe(mongoRef.current)
+  }, [])
 
   return (
     <section className={styles.container}>
-      <h2 ref={titleRef} style={styleTest} className={styles.sectionTitle}>
+      <h2 ref={titleRef} className={titleClass}>
         À propos
       </h2>
-      <div className={styles.aboutMe}>
+      <div ref={aboutMeRef} className={aboutMeClass}>
         <div className={styles.aboutMeText}>
           <h3 className={styles.h3}>
             Salut ! Je suis Ryan et je suis un{" "}
@@ -40,7 +189,7 @@ export default function About() {
             intelligentes, ce qui conduit à une riche expérience web pour
             l'utilisateur final. Lorsque je ne travaille pas sur le code, je
             suis toujours à la recherche d'apprendre plus et de relever de
-            nouveaux défis. Je suis également disponible pour un emploi.
+            nouveaux défis.
           </p>
         </div>
 
@@ -54,9 +203,11 @@ export default function About() {
         <span className={styles.bgGlowing}></span>
       </div>
 
-      <h2 className={styles.sectionTitle2}>Compétences</h2>
+      <h2 ref={title2Ref} className={title2Class}>
+        Compétences
+      </h2>
       <div className={styles.skillsContainer}>
-        <div className={`${styles.skillCard} ${styles.skillCard1}`}>
+        <div ref={reactRef} className={reactClass}>
           <img
             src="ReactIcon.png"
             className={styles.skillIcon}
@@ -64,7 +215,7 @@ export default function About() {
           />
           <p>React / React Native</p>
         </div>
-        <div className={`${styles.skillCard} ${styles.skillCard2}`}>
+        <div ref={nextRef} className={nextClass}>
           <img
             src="ImgIconNext.png"
             className={styles.skillIcon}
@@ -72,7 +223,7 @@ export default function About() {
           />
           <p>NextJs</p>
         </div>{" "}
-        <div className={`${styles.skillCard} ${styles.skillCard3}`}>
+        <div ref={NodeRef} className={nodeClass}>
           <img
             src="NodeJsIcon.png"
             className={styles.skillIcon}
@@ -80,7 +231,7 @@ export default function About() {
           />
           <p>NodeJs</p>
         </div>{" "}
-        <div className={`${styles.skillCard} ${styles.skillCard4}`}>
+        <div ref={expressRef} className={expressClass}>
           <img
             src="JsIcon.png"
             className={styles.skillIcon}
@@ -88,7 +239,7 @@ export default function About() {
           />
           <p>ExpressJs</p>
         </div>{" "}
-        <div className={`${styles.skillCard} ${styles.skillCard5}`}>
+        <div ref={htmlRef} className={htmlClass}>
           <img
             src="HtmlIcon.png"
             className={styles.skillIcon}
@@ -96,15 +247,15 @@ export default function About() {
           />
           <p>HTML</p>
         </div>{" "}
-        <div className={`${styles.skillCard} ${styles.skillCard6}`}>
+        <div ref={cssRef} className={cssClass}>
           <img src="CssIcon.png" className={styles.skillIcon} alt="CSS Logo" />
           <p>CSS</p>
         </div>{" "}
-        <div className={`${styles.skillCard} ${styles.skillCard7}`}>
+        <div ref={gitRef} className={gitClass}>
           <img src="GitIcon.png" className={styles.skillIcon} alt="Git Logo" />
           <p>Git / Github</p>
         </div>{" "}
-        <div className={`${styles.skillCard} ${styles.skillCard8}`}>
+        <div ref={mongoRef} className={mongoClass}>
           <img
             src="MongoDbIcon.png"
             className={styles.skillIcon}
@@ -114,5 +265,5 @@ export default function About() {
         </div>
       </div>
     </section>
-  );
+  )
 }
--- components/Contact.js
+++ components/Contact.js
@@ -1,128 +1,17 @@
-import styles from "@/styles/Contact.module.css";
-
-import { useState } from "react";
+import styles from "@/styles/Contact.module.css"
 
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [isSend, setIsSend] = useState("");
-  const [isSendBool, setIsSendBool] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const sendMessage = () => {
-    if (isLoading) {
-      return;
-    }
-    if (
-      !email.match(
-        /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/
-      )
-    ) {
-      setIsSend("Votre email n'est pas valable");
-      setIsSendBool(false);
-      return;
-    }
-    if (name && email && message) {
-      setIsLoading(true);
-      fetch("https://portfolio-backend-puce.vercel.app/message", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          message: message,
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.result) {
-            setIsSend("Message envoyé avec succès !");
-            setEmail("");
-            setMessage("");
-            setName("");
-            setIsSendBool(true);
-            setIsLoading(false);
-          } else {
-            setIsSend("Une erreur à eu lieu réessayez ou envoyez moi un mail");
-            setIsSendBool(false);
-          }
-        });
-    } else {
-      setIsSend("Remplissez tous les champs");
-      setIsSendBool(false);
-    }
-  };
-  let isSendStyle = { margin: "20px 0 0", color: "red" };
-  if (isSendBool) {
-    isSendStyle = { margin: "20px 0 0", color: "green" };
-  }
-
   return (
     <section className={styles.container}>
       <h2 className={styles.h2}>Contact</h2>
       <div className={styles.contactSection}>
         <p className={styles.p}>
           Si vous avez une opportunité ou un message à me faire passer,
-          n'hésitez pas à me l'envoyer via le formulaire ou par mail.
+          n'hésitez pas à me contacter par mail ou par Linkedin.
           <br />
           Je suis toujours à l'écoute de nouvelles opportunités et je me ferai
           un plaisir de vous répondre dès que possible.
         </p>
-        <form className={styles.form}>
-          <label htmlFor="inputName" className={styles.label}>
-            Nom
-          </label>
-          <input
-            id="inputName"
-            type="text"
-            className={styles.input}
-            placeholder="Nom"
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            maxLength="50"
-            autoComplete="name"
-          />
-          <label htmlFor="inputMail" className={styles.label}>
-            Email
-          </label>
-          <input
-            id="inputMail"
-            type="email"
-            className={styles.input}
-            placeholder="Email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            maxLength="50"
-          />
-          <label htmlFor="inputMsg" className={styles.label}>
-            Message
-          </label>
-          <textarea
-            id="inputMsg"
-            type="text"
-            className={styles.input}
-            placeholder="Entrez votre message"
-            required
-            style={{ height: "200px" }}
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            maxLength="1000"
-          />
-          {isLoading && <div className={styles.loader} />}
-          <p style={isSendStyle} className={styles.pStatus}>
-            {isSend}
-          </p>
-          <button
-            type="button"
-            className={styles.btnForm}
-            onClick={sendMessage}
-          >
-            Envoyer
-          </button>
-        </form>
       </div>
       <footer className={styles.footer}>
         <a href="https://github.com/RyanRml" target="_blank" title="Github">
@@ -148,5 +37,5 @@ export default function Contact() {
         </a>
       </footer>
     </section>
-  );
+  )
 }
